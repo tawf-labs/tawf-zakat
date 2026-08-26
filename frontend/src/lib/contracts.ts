0ad64ea5ef0bdc129e2a538beb0a97028b94f1cf
harkon666 <bryan.wicaksanaa@gmail.com>
2026-08-26 15:14:16 +0700

    feat(deploy): deploy ZakatProtocolL1 live to Ethereum Sepolia and configure contract addresses

---
+++ frontend/src/lib/contracts.ts
@@ -0,0 +1,124 @@
+export const ZAKAT_PROTOCOL_L1_ADDRESS = "0x72b60a0C37a78dF62295F88294E790083089f665" as const;
+export const SEPOLIA_USDC_ADDRESS = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238" as const;
+
+export const SEPOLIA_CHAIN_ID = 11155111;
+export const SEPOLIA_EXPLORER_URL = "https://sepolia.etherscan.io";
+
+export const ZAKAT_PROTOCOL_ABI = [
+  {
+    type: "function",
+    name: "depositUSDC",
+    inputs: [
+      { name: "_amountUSDC", type: "uint256" },
+      { name: "_isAnonymous", type: "bool" },
+      { name: "_anonymousCommitment", type: "bytes32" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "recordFiatBatchSettlement",
+    inputs: [
+      { name: "_batchId", type: "uint256" },
+      { name: "_merkleRoot", type: "bytes32" },
+      { name: "_totalBatchAmountIDR", type: "uint256" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "proposeDisbursement",
+    inputs: [
+      { name: "_currencyType", type: "uint8" },
+      { name: "_amount", type: "uint256" },
+      { name: "_asnafCategory", type: "uint8" },
+      { name: "_beneficiaryHash", type: "bytes32" },
+      { name: "_ipfsProofCID", type: "string" },
+      { name: "_periodId", type: "uint256" },
+      { name: "_usdcRecipient", type: "address" },
+    ],
+    outputs: [{ name: "proposalId", type: "uint256" }],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "approveDisbursement",
+    inputs: [{ name: "_proposalId", type: "uint256" }],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "cancelProposal",
+    inputs: [
+      { name: "_proposalId", type: "uint256" },
+      { name: "_reason", type: "string" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "executeDisbursement",
+    inputs: [{ name: "_proposalId", type: "uint256" }],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "withdrawAmilShareUSDC",
+    inputs: [
+      { name: "_to", type: "address" },
+      { name: "_amount", type: "uint256" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "MAX_AMIL_BPS",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "fiatBatchRoots",
+    inputs: [{ name: "", type: "uint256" }],
+    outputs: [{ name: "", type: "bytes32" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "mustahikVaultIDR",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "mustahikVaultUSDC",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "amilTreasuryUSDC",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "hasReceivedZakat",
+    inputs: [
+      { name: "", type: "bytes32" },
+      { name: "", type: "uint256" },
+    ],
+    outputs: [{ name: "", type: "bool" }],
+    stateMutability: "view",
+  },
+] as const;
